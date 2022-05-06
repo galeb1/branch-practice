e93d1b14529ed0a50c02cd111eaddc97d496a5ff
galeb1 <galeb12243@gmail.com>
2022-05-06 15:14:29 +0900

    feat: Print dream if i is times of 5
    Other

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
-for i in range(1,17):
-    if i%15== 0:
-        print('yeardream')
-    elif i%3==0:
+for i in range(1,26):
+    if i%3== 0:
         print('year')
+    elif i%5==0:
+        print('dream')
     else:
         print(i)
 
