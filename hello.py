dc38fc47d103ebad480cd218b088d673cd8444d2
galeb1 <galeb12243@gmail.com>
2022-05-06 15:18:13 +0900

    feat: Print yeardream if i is times of 15
    Other

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
 for i in range(1,26):
-    if i%3== 0:
+    if i%15== 0:
+        print('yeardream')
+    elif i%3==0:
         print('year')
-    elif i%5==0:
-        print('dream')
     else:
         print(i)
 
